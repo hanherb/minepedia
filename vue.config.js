f542032b3e342cc1132397a85ec61be9e6c17fc4
hanherb <dyo.pyon@gmail.com>
2019-07-25 20:45:56 +0700

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   	baseUrl: '/admin/',
- //  	devServer: {
- //        host: 'qrcode.wearesqood.com',
- //        port: 8080,
-	// },
+  	devServer: {
+        host: 'qrcode.wearesqood.com',
+        port: 8080,
+	},
   	transpileDependencies: ['shards-vue'],
 };
